6b6120da8986ce2a89a6cb39f6305bc946b5ee83
SpacingBat3 <git@spacingbat3.anonaddy.com>
2024-02-15 17:56:31 +0100

    Init repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# AUR packages collection
+
+A GitHub project with a collection of AUR packages and tools, to support my
+AUR maintenance.
---
+++ tools/check-updates.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/sh -e
+# shellcheck shell=sh disable=SC2086
+
+CSD="${0%/*}"
+PKGDIR="$CSD/../packages"
+
+VSEL='^pkgver='
+STDIN=/proc/$$/fd/0
+
+updsrcinfo() (
+	cd "$PKGDIR/$1";
+	makepkg --printsrcinfo > .SRCINFO
+)
+
+normalize_version() {
+	version=
+	while read version; do
+		if [ "$(echo "$version" | cut -c1)" = "v" ]; then
+			version="$(echo "$version" | cut -c 2-)";
+		fi
+		if [ -n "$1" ]; then
+			version="$(echo "$version" | sed 's~'"$1"'~~g')";
+		fi
+		echo "$version"
+	done
+	unset version;
+}
+
+git_tag_latest() {
+	version=$(git ls-remote --exit-code --refs --tags $1 $2'*');
+	echo "$version" | cut -d '/' -f 3 | normalize_version $2 | \
+		sort -V | tail -n1;
+    unset version;
+}
+
+fetch_version() {
+	case $1 in
+		github) git_tag_latest "https://github.com/$2.git" $3;;
+		git) git_tag_latest "$2" $3 ;;
+		*) return 1 ;;
+	esac;
+	return 0;
+}
+
+handle_line() {
+	pkgbuild="$PKGDIR/$1/PKGBUILD"
+	version=$(
+		grep -E $VSEL "$pkgbuild" | \
+			sed -E "s/$VSEL(.*)$/\\1/g" | \
+			tail -n1 | \
+			normalize_version $3
+	);
+	version_new="$(fetch_version $2 $3 $4)"
+	if [ -n "$version_new" ] && [ "$version" != "$version_new" ]; then
+		echo "$1: Update from $version to $version_new...";
+		sed -i.old "s/$VSEL$version$/pkgver=$version_new/g" "$pkgbuild";
+		echo "$1: Updating checksums for the new sources...";
+		updpkgsums "$pkgbuild";
+		echo "$1: Updating .SRCINFO from the new PKGBUILD...";
+		updsrcinfo "$1";
+		printf '%s\n' \
+			"$1: Manual interaction required (commiting, pushing repo)!" \
+			"    A new shell instance will be opened to commit and push changes..." \
+			"    (Type 'exit' to continue updating another packages.)"
+		(
+			cd "$PKGDIR/$1";
+			"$SHELL" < $STDIN;
+			echo "$1: Shell returned code $?."
+		)
+	elif [ -n "$version_new" ]; then
+		echo "$1: pkgver=$version is up-to-date";
+	fi
+	unset version version_new
+}
+
+(
+	while read line; do if [ -n "$line" ] && [ "_$(echo "$line" | cut -c1)" != "_#" ]; then
+		handle_line $line;
+	fi; done < "$PKGDIR/UPDATESRC";
+	echo
+	echo "Done all jobs for now. :)"
+)
+unset line;
